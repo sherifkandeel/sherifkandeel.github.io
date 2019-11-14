e97d7ae4d8d969b771d261d570a6381daaf9cca7
Sherif Mohamed <sherif.mohamed@deliveryhero.com>
2019-11-14 02:38:37 +0100

    add things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const item = localStorage.getItem('data');
 function App() {
+    const [data, setData] = useState(localStorage.getItem('data'));
+
+    useEffect(() => {
+ localStorage.setItem('data', data)
+    }, [data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link href='https://fonts.googleapis.com/css?family=Roboto Mono' rel='stylesheet'/>
+      <textarea cols="100" rows="50" value={data} class="textarea-css" placeholder="add some data" onChange={e =>  setData(e.target.value)} />
     </div>
   );
 }
