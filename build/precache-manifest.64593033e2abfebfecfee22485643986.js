39fd27c907ad147735db1a02e29948de27a4caad
Sherif Mohamed <sherif.mohamed@deliveryhero.com>
2019-11-27 17:14:41 +0100

    added build files

---
+++ build/precache-manifest.64593033e2abfebfecfee22485643986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea61ec3ac5910ab98d4107fbeab4c551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3620ba34ccc7e7a58e",
+    "url": "/static/css/main.3fadf9ce.chunk.css"
+  },
+  {
+    "revision": "33ee9b8ee5e0c6de7089",
+    "url": "/static/js/2.13c1e6d4.chunk.js"
+  },
+  {
+    "revision": "ec3620ba34ccc7e7a58e",
+    "url": "/static/js/main.ac06bd84.chunk.js"
+  },
+  {
+    "revision": "d5e52aa8fc9fdee65928",
+    "url": "/static/js/runtime-main.14ef7f66.js"
+  }
+]);
